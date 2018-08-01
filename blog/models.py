b21485bef208472bdb54122c13d7b309f33d4e46
jlochran <jlochran@gmail.com>
2018-07-31 22:28:02 -0400

    added front end

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=72, blank=True)
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    pub_date = models.DateTimeField()
