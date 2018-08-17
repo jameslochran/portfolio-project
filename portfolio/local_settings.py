d98afb445616dfd9b173d64415235cbdc8bd96b8
jlochran <jlochran@gmail.com>
2018-08-17 15:49:34 -0400

    sk change

--- portfolio/local_settings.py
+++ portfolio/local_settings.py
@@ -1,4 +1,4 @@
-SECRET_KEY = '#@w7*a)12_d-e1r%uo(=45p3l(x13zp2m#1t8a1$b6kurv^^'
+SECRET_KEY = '#@w7*a)12_d-e1r%uo(=45p3l(x13zp2m#1t8a1$b6kurv^^hotdoge5'
 
 ALLOWED_HOSTS = ['167.99.154.197', 'jlochran.com', 'www.jlochran.com']
 
